daf1f86029bb73f3e4ac841b6803052c5b4c555d
Dina Ramaro <ramaro.dina@gmail.com>
2019-10-18 18:08:15 +0200

    front slack channels

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -8,21 +8,59 @@ import {
   Badge
 } from "reactstrap";
 import "./App.css";
+import iconPlus from "./images/plus.png";
 
 function App() {
   return (
-    <Container>
+    <Container style={{ maxWidth: "none" }}>
       <Row>
         <Col sm="2">
-          <ListGroup>
-            <ListGroupItem className="justify-content-between">
-              Cras justo odio <Badge pill>14</Badge>
+          <ListGroup style={{ color: "#ffffff", fontWeight: "bold" }}>
+            <ListGroupItem
+              className="justify-content-between"
+              style={{
+                backgroundColor: "#480042",
+                borderStyle: "hidden"
+              }}
+            >
+              Chaînes
+              <img src={iconPlus} alt="plus" style={{ float: "right" }} />
             </ListGroupItem>
-            <ListGroupItem className="justify-content-between">
-              Dapibus ac facilisis in <Badge pill>2</Badge>
+            <ListGroupItem
+              className="justify-content-between"
+              style={{ backgroundColor: "#480042", borderStyle: "hidden" }}
+            >
+              # corrections{" "}
+              <Badge pill style={{ backgroundColor: "#DC004F" }}>
+                14
+              </Badge>
             </ListGroupItem>
-            <ListGroupItem className="justify-content-between">
-              Morbi leo risus <Badge pill>1</Badge>
+            <ListGroupItem
+              className="justify-content-between"
+              style={{ backgroundColor: "#480042", borderStyle: "hidden" }}
+            >
+              # events{" "}
+              <Badge pill style={{ backgroundColor: "#DC004F" }}>
+                14
+              </Badge>
+            </ListGroupItem>
+            <ListGroupItem
+              className="justify-content-between"
+              style={{ backgroundColor: "#480042", borderStyle: "hidden" }}
+            >
+              # general{" "}
+              <Badge pill style={{ backgroundColor: "#DC004F" }}>
+                2
+              </Badge>
+            </ListGroupItem>
+            <ListGroupItem
+              className="justify-content-between"
+              style={{ backgroundColor: "#480042", borderStyle: "hidden" }}
+            >
+              # paris_alternance{" "}
+              <Badge pill style={{ backgroundColor: "#DC004F" }}>
+                1
+              </Badge>
             </ListGroupItem>
           </ListGroup>
         </Col>
